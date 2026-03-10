7fdad60b8ced7897243946a360fdbc0674ed008f
bediakopatrick054-afk <bediakopatrick054@gmail.com>
2026-03-10 21:03:32 +0000

    Update migration.sql
    updated

--- prisma/migrations/20240913083652_addbirthday/migration.sql
+++ prisma/migrations/20240913083652_addbirthday/migration.sql
@@ -1,21 +1,30 @@
 /*
   Warnings:
 
-  - Added the required column `birthday` to the `Student` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `birthday` to the `Teacher` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `birthday` to the `Member` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `birthday` to the `Pastor` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
-ALTER TABLE "Class" DROP CONSTRAINT "Class_supervisorId_fkey";
+ALTER TABLE "CellGroup" DROP CONSTRAINT "CellGroup_leaderId_fkey";
 
 -- AlterTable
-ALTER TABLE "Class" ALTER COLUMN "supervisorId" DROP NOT NULL;
+ALTER TABLE "CellGroup" ALTER COLUMN "leaderId" DROP NOT NULL;
 
 -- AlterTable
-ALTER TABLE "Student" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "Member" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
 
 -- AlterTable
-ALTER TABLE "Teacher" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "Pastor" ADD COLUMN     "birthday" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Ministry" ADD COLUMN     "leaderId" TEXT;
 
 -- AddForeignKey
 ALTER TABLE "Class" ADD CONSTRAINT "Class_supervisorId_fkey" FOREIGN KEY ("supervisorId") REFERENCES "Teacher"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CellGroup" ADD CONSTRAINT "CellGroup_leaderId_fkey" FOREIGN KEY ("leaderId") REFERENCES "Pastor"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Ministry" ADD CONSTRAINT "Ministry_leaderId_fkey" FOREIGN KEY ("leaderId") REFERENCES "Pastor"("id") ON DELETE SET NULL ON UPDATE CASCADE;
